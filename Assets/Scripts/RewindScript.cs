df3032558510f2b63652bdceda0250f5ed29e5d3
Grammar <mt.favhwdg@gmail.com>
2020-08-01 16:46:54 +0300

    Changes made to script and level

--- Assets/Scripts/RewindScript.cs
+++ Assets/Scripts/RewindScript.cs
@@ -31,7 +31,7 @@ public class RewindScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey(RewindKey))
+		if (Input.GetKeyDown(RewindKey))
 		{
 			RewindFunction();
 		}
