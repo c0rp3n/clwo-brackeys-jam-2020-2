b216e27a361e77325ae267808ed81855235f909d
Grammar <mt.favhwdg@gmail.com>
2020-08-01 16:03:31 +0300

    Rewind Function

---
+++ Assets/Scripts/RewindScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindScript : MonoBehaviour
+{
+	public KeyCode RewindKey = KeyCode.R;
+	public GameObject PointOfRewind;
+	public int NumberofRewindsLeft=1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		PointOfRewind = GameObject.Find("RewindPoint");
+    }
+
+	void RewindFunction()
+	{
+		if (NumberofRewindsLeft > 0)
+		{
+			Debug.Log("Rewound from: " + gameObject.transform.position + "To: " + PointOfRewind.transform.position);
+			gameObject.transform.position = PointOfRewind.transform.position;
+			NumberofRewindsLeft--;
+		}
+		else
+		{
+			Debug.Log("Did not go in rewind func, current number of rewinds: " + NumberofRewindsLeft);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(RewindKey))
+		{
+			RewindFunction();
+		}
+    }
+}
